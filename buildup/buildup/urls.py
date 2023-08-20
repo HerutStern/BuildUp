4b0627810c3dd2d762c6cc2f565eea238c366019
HERUT <herut.shtern@gmail.com>
2023-08-20 22:53:29 +0300

    BuildUp

--- buildup/buildup/urls.py
+++ buildup/buildup/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for buildup project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,10 +5,20 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Users:
     path('api/buildup/', include('buildup_app.users.urls')),
+
+    # Company Files:
     path('api/buildup/', include('buildup_app.company_file.urls')),
+
+    # File Template:
     path('api/buildup/', include('buildup_app.file_template.urls')),
+
+    # Section Template:
     path('api/buildup/', include('buildup_app.section_template.urls')),
+
+    # Building Permit:
     path('api/buildup/', include('buildup_app.building_permit.urls'))
 ]
 
