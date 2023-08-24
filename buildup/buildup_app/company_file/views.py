b8582eb5bc2822707ad21cfe42d9a0e45d8296bb
HERUT <herut.shtern@gmail.com>
2023-08-24 21:28:08 +0300

    BuildUp

--- buildup/buildup_app/company_file/views.py
+++ buildup/buildup_app/company_file/views.py
@@ -87,3 +87,4 @@ class CompanyFileViewSet(mixins.CreateModelMixin,
             self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
